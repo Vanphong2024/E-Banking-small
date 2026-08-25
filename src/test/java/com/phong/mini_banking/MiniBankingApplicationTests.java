f2a66e13daeb72491a6bc6ff6ffe20cdcfcd9c8d
Vanphong2024 <nguyenvanphong231205@gmail.com>
2026-08-25 10:07:24 +0700

    push_code

---
+++ src/test/java/com/phong/mini_banking/MiniBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phong.mini_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
